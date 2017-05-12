f033e3022cc1ad29ce0492729943c41b31eed650
Lauri Lahti <lauri.lahti@outlook.com>
2017-05-12 10:09:59 +0300

    Download also the small subset of data.

--- initialize.sh
+++ initialize.sh
@@ -10,12 +10,21 @@ echo "Create empty directory $DATAPATH"
 mkdir $DATAPATH
 cd $DATAPATH
 
-echo "Download training data..."
+echo "Download subset of training data..."
+wget https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/vehicles_smallset.zip -q --show-progress
+unzip -q vehicles_smallset.zip
+rm vehicles_smallset.zip
+wget https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/non-vehicles_smallset.zip -q --show-progress
+unzip -q non-vehicles_smallset.zip
+rm non-vehicles_smallset.zip
+
+echo "Download full set of training data..."
 wget https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/vehicles.zip -q --show-progress
 unzip -q vehicles.zip
 rm vehicles.zip
 wget https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/non-vehicles.zip -q --show-progress
 unzip -q non-vehicles.zip
 rm non-vehicles.zip
+
 echo ""
 echo "Done!"
