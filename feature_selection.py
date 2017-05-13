09e25ba9bbf4d88610ce2628483578c00e85f5f6
Lauri Lahti <lauri.lahti@outlook.com>
2017-05-13 14:54:23 +0300

    Moved single_img_features() into lesson_functions.py

--- feature_selection.py
+++ feature_selection.py
@@ -1,6 +1,10 @@
-# This code is based on udacity example
-# https://classroom.udacity.com/nanodegrees/nd013/parts/fbf77062-5703-404e-b60c-95b78b2f3f9e/modules/2b62a1c3-e151-4a0e-b6b6-e424fa46ceab/lessons/fd66c083-4ccb-4fe3-bda1-c29db76f50a0/concepts/40ac880a-7ccc-4145-a864-6b0b99ea31e9
+""" Helper for feature selection. By using this you are able to examine several 
+features.
 
+This code is based on udacity example
+https://classroom.udacity.com/nanodegrees/nd013/parts/fbf77062-5703-404e-b60c-95b78b2f3f9e/modules/2b62a1c3-e151-4a0e-b6b6-e424fa46ceab/lessons/fd66c083-4ccb-4fe3-bda1-c29db76f50a0/concepts/40ac880a-7ccc-4145-a864-6b0b99ea31e9
+
+"""
 
 import time
 from sklearn.svm import LinearSVC
@@ -13,60 +17,6 @@ from dataset import *
 from utils import *
 import pandas as pd
 import tqdm
-
-
-# Define a function to extract features from a single image window
-# This function is very similar to extract_features()
-# just for a single image rather than list of images
-def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
-                        hist_bins=32, orient=9,
-                        pix_per_cell=8, cell_per_block=2, hog_channel=0,
-                        spatial_feat=True, hist_feat=True, hog_feat=True):
-    # 1) Define an empty list to receive features
-    img_features = []
-    # 2) Apply color conversion if other than 'RGB'
-    if color_space != 'RGB':
-        if color_space == 'HSV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        elif color_space == 'LUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-        elif color_space == 'HLS':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-        elif color_space == 'YUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-        elif color_space == 'YCrCb':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-    else:
-        feature_image = np.copy(img)
-    # 3) Compute spatial features if flag is set
-    if spatial_feat == True:
-        spatial_features = bin_spatial(feature_image, size=spatial_size)
-        # 4) Append features to list
-        img_features.append(spatial_features)
-    # 5) Compute histogram features if flag is set
-    if hist_feat == True:
-        hist_features = color_hist(feature_image, nbins=hist_bins)
-        # 6) Append features to list
-        img_features.append(hist_features)
-    # 7) Compute HOG features if flag is set
-    if hog_feat == True:
-        if hog_channel == 'ALL':
-            hog_features = []
-            for channel in range(feature_image.shape[2]):
-                hog_features.extend(
-                    get_hog_features(feature_image[:, :, channel],
-                                     orient, pix_per_cell, cell_per_block,
-                                     vis=False, feature_vec=True))
-        else:
-            hog_features = get_hog_features(feature_image[:, :, hog_channel],
-                                            orient,
-                                            pix_per_cell, cell_per_block,
-                                            vis=False, feature_vec=True)
-        # 8) Append features to list
-        img_features.append(hog_features)
-
-    # 9) Return concatenated array of features
-    return np.concatenate(img_features)
 
 
 save_results = True
